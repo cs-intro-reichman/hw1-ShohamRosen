b46e3a799b8a7c3e37469c912565fa7e8ab2dbff
ShohamRosen <shoham.rosen@post.runi.ac.il>
2025-11-04 10:56:39 +0200

    fix bill3

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
 		String name3 = args[2]; //the user will input a name 
 		int bill = Integer.parseInt(args[3]); //the user will input the value of the bill 
 		double splitBill = Math.ceil((double) bill / 3);   //the program will take the number, divide it to 3 and will use the function ceil to round. it up
-		System.out.println("Dear " + name1 + ", " + name2 + ", " + "and " + name3 + ": " + "pay " + splitBill + " Shekels each.");   //print the names and the. values to
+		System.out.println("Dear " + name3 + ", " + name2 + ", " + "and " + name1 + ": " + "pay " + splitBill + " Shekels each.");   //print the names and the. values to
 		
 	}
 }
